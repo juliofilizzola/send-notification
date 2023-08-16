e2ec0991ffe5f7102fa29f7719d6fac436a187d1
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-08-15 23:02:10 -0300

    test: `ref` ->  modify code test service and ref code `clean code`

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -12,36 +12,23 @@ type repositoryMock struct {
 	mock.Mock
 }
 
+var (
+	newCampaign = dto.NewCampaign{
+		Content: "this is test service",
+		Contact: []string{"email@test.com", "email2@test.com"},
+		Name:    "Test Service",
+	}
+	service = Service{}
+)
+
 func (r *repositoryMock) Save(campaign *Campaign) error {
 	args := r.Called(campaign)
 	return args.Error(0)
 }
 
-func TestService_Create(t *testing.T) {
-	assertions := assert.New(t)
-	service := Service{
-		Repository: new(repositoryMock),
-	}
-
-	newCampaign := dto.NewCampaign{
-		Content: "this is test service",
-		Contact: []string{"email@test.com", "email2@test.com"},
-		Name:    "Test Service",
-	}
-
-	id, err := service.Create(newCampaign)
-	assertions.Nil(err)
-	assertions.NotNil(id)
-}
-
 func TestService_SaveCampaign(t *testing.T) {
 	assertions := assert.New(t)
 	repositoryMock := new(repositoryMock)
-	newCampaign := dto.NewCampaign{
-		Content: "this is test service",
-		Contact: []string{"email@test.com", "email2@test.com"},
-		Name:    "Test Service",
-	}
 
 	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
 		if campaign.Name != newCampaign.Name {
@@ -53,10 +40,7 @@ func TestService_SaveCampaign(t *testing.T) {
 		}
 		return true
 	})).Return(nil)
-
-	service := Service{
-		Repository: repositoryMock,
-	}
+	service.Repository = repositoryMock
 
 	id, err := service.Create(newCampaign)
 	repositoryMock.AssertExpectations(t)
