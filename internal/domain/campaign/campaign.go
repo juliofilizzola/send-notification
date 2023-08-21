98d7e46fd37cc652e45ce6c9d4ceb3e1b6100db4
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-08-20 23:00:17 -0300

    feat: add validate from struct

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,35 +1,24 @@
 package campaign
 
 import (
-	"errors"
 	"time"
 
 	"github.com/rs/xid"
 )
 
 type Contact struct {
-	Email string `json:"email"`
+	Email string `validate:"email"`
 }
 
 type Campaign struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	Contact   []Contact `json:"contact"`
+	ID        string    `validate:"required"`
+	Name      string    `validate:"min=5,max=24"`
+	Content   string    `validate:"min=5,max=1024"`
+	CreatedAt time.Time `validate:"required"`
+	Contact   []Contact `validate:"min=1,dive"`
 }
 
 func NewCampaign(name, content string, emails []string) (*Campaign, error) {
-	if name == "" {
-		return nil, errors.New("name is required")
-	}
-
-	if content == "" {
-		return nil, errors.New("content is required")
-	}
-	if len(emails) == 0 {
-		return nil, errors.New("at least one email has to be provided")
-	}
 	contacts := make([]Contact, len(emails))
 
 	for index, email := range emails {
